42115242f044d952c36860e7a0d10577eaea1f27
John Charles Woods <johncharleswoods@protonmail.com>
2026-09-19 08:39:11 -0400

    Forked EagletOS-DWM from TinyWM

--- __main__.py
+++ __main__.py
@@ -8,12 +8,25 @@ from Xlib import X, XK
 
 dpy = Display()
 
-dpy.screen().root.grab_key(dpy.keysym_to_keycode(XK.string_to_keysym("F1")), X.Mod1Mask, 1,
-        X.GrabModeAsync, X.GrabModeAsync)
-dpy.screen().root.grab_button(1, X.Mod1Mask, 1, X.ButtonPressMask|X.ButtonReleaseMask|X.PointerMotionMask,
-        X.GrabModeAsync, X.GrabModeAsync, X.NONE, X.NONE)
+dpy.screen().root.grab_key(
+    dpy.keysym_to_keycode(XK.string_to_keysym("F1")), # Alt+F1
+    X.Mod1Mask, 
+    1,
+    X.GrabModeAsync, 
+    X.GrabModeAsync)
+dpy.screen().root.grab_button(
+    1, # Left click
+    X.Mod1Mask, 
+    1, 
+    X.ButtonPressMask|X.ButtonReleaseMask|X.PointerMotionMask,
+    X.GrabModeAsync, 
+    X.GrabModeAsync, 
+    X.NONE, 
+    X.NONE
+)
 dpy.screen().root.grab_button(3, X.Mod1Mask, 1, X.ButtonPressMask|X.ButtonReleaseMask|X.PointerMotionMask,
         X.GrabModeAsync, X.GrabModeAsync, X.NONE, X.NONE)
+dpy.screen().root.grab_key()
 
 start = None
 while 1:
